f74b2b17a312fa506c5b3d9809ae4ba6c49bd821
Ben Taylor <ben.taylor.uk@gmail.com>
2012-11-19 15:01:49 +0000

    WIP on Nancy

--- Cantos/Nancy.fs
+++ Cantos/Nancy.fs
@@ -5,19 +5,44 @@ open Nancy.Conventions
 open Nancy
 open System
 
-type StaticSiteBootstrapper(path) =
+let mutable maybeRootPath = None
 
-    inherit DefaultNancyBootstrapper()
-    with override x.ConfigureConventions(conventions) =
-            base.ConfigureConventions(conventions)
-            let convention = StaticContentConventionBuilder.AddDirectory("/", path)
+
+type RootPathProvider() =
+
+    let path = 
+        match maybeRootPath with
+        | None -> raiseInvalidOp "path is not set.  Which is an ugly hack anyway due to Nancy design or my lack of Nancy knowledge."
+        | Some(path) -> path
+
+    interface IRootPathProvider with
+        member x.GetRootPath() = path
+        member x.Equals(obj) = 
+            match obj with
+            | null -> false
+            | :? IRootPathProvider as rpp -> rpp.GetRootPath() = path
+            | _ -> false
+        member x.GetHashCode() = x.GetHashCode()
+        member x.GetType() = x.GetType()
+        member x.ToString() = path
+
+type StaticSiteBootstrapper() =
+    inherit DefaultNancyBootstrapper() with
+        override x.ConfigureConventions(conventions) =
+            let convention = StaticContentConventionBuilder.AddDirectory("/","")
+            System.Diagnostics.Debugger.Break()
             let _ = conventions.StaticContentsConventions.Add(convention)
+            base.ConfigureConventions(conventions)
             ()
+        override x.DiagnosticsConfiguration = Nancy.Diagnostics.DiagnosticsConfiguration(Password = @"goldfish")
+        override x.RootPathProvider = typeof<RootPathProvider>
 
 let runPreviewServer path (port:int) = 
+    //TODO get rid of this mutation.
+    maybeRootPath <- Some(path)
 
     let nancyHost = 
         let uri = Uri(sprintf "http://localhost:%i" port)
-        NancyHost(uri, StaticSiteBootstrapper(path))
+        NancyHost(uri, StaticSiteBootstrapper())
 
     nancyHost.Start()
